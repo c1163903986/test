9c85ce1f796badd78c3136c0cd79d1444eebd40b
c1163903986 <1163903986@qq.com>
2020-07-18 11:56:25 +0800

    mvn first commit

---
+++ src/main/java/cn/kgc/App.java
@@ -0,0 +1,18 @@
+package cn.kgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 
+	public Integer add(Integer a,Integer b){
+		return a+b;
+	} 
+	 
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello TCST003!" );
+    }
+}
